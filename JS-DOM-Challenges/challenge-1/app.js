8c00e8f56b78b4b1f6b2561ebc6de3f8be9550ff
CoderNandan <nandanindia1947@gmail.com>
2025-02-23 14:25:39 +0530

    challenge-2 done

--- JS-DOM-Challenges/challenge-1/app.js
+++ JS-DOM-Challenges/challenge-1/app.js
@@ -4,15 +4,18 @@
 const button = document.getElementById("toggleButton");
 const bulb = document.getElementById("bulb");
 const state = document.getElementById("status");
+const body = document.querySelector("body");
 
 button.addEventListener("click", () => {
   if (bulb.classList.contains("off")) {
     bulb.classList.remove("off");
+    body.classList.remove("dark-mode");
     button.innerText = "Turn Off";
     state.innerHTML = "Status: On";
   } else {
     bulb.classList.add("off");
     button.innerText = "Turn On";
+    body.classList.add("dark-mode");
     state.innerHTML = "Status: Off";
   }
 });
--- JS-DOM-Challenges/challenge-2/app.js
+++ JS-DOM-Challenges/challenge-2/app.js
@@ -1,3 +1,15 @@
 /**
  * Write your challenge solution here
  */
+const text = document.getElementById("mainHeading");
+const buttons = document.querySelectorAll(".color-buttons button");
+console.log(buttons);
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    console.log("hi");
+    if (button.id === "resetButton") {
+      text.style.color = "";
+    }
+    text.style.color = button.innerHTML.toLowerCase();
+  });
+});
