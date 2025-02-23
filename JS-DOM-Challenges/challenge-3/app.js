cb0bafef50e95047e5bde24e443ae79db686c847
CoderNandan <nandanindia1947@gmail.com>
2025-02-23 14:55:27 +0530

    challenge-3 done

--- JS-DOM-Challenges/challenge-3/app.js
+++ JS-DOM-Challenges/challenge-3/app.js
@@ -1,3 +1,17 @@
 /**
  * Write your challenge solution here
  */
+const inputs = document.querySelectorAll(".form-group input");
+const bioInput = document.getElementById("bioInput");
+const outPutsIds = ["nameDisplay", "jobDisplay", "ageDisplay", "bioDisplay"];
+
+inputs.forEach((input, index) => {
+  input.addEventListener("input", () => {
+    const field = document.getElementById(`${outPutsIds[index]}`);
+    field.innerText = input.value || "Not provided";
+  });
+});
+bioInput.addEventListener("input", () => {
+  const field = document.getElementById(`${outPutsIds[3]}`);
+  field.innerText = bioInput.value || "Not provided";
+});
